1668aad731f923e7707fcf655792c26b612d5762
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-07-12 15:19:22 -0600

    BuildContext y Scaffold

--- counter_app_01/lib/screens/home_screen.dart
+++ counter_app_01/lib/screens/home_screen.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 class HomeSCreen extends StatelessWidget {
   const HomeSCreen({super.key});
 
+  // El BuildContext es todo el arbol de Widget
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Hola Desde HomeScreen'));
+    // El Scaffold es un Widget que crea toda una pantalla en blanco para nosotros poder añadir estilos. Es como un lienzo para empezar a poner Widget
+    return const Scaffold(
+      // backgroundColor: Colors.pink,
+      body: Center(child: Text('Hola Desde HomeScreen')),
+    );
   }
 }
