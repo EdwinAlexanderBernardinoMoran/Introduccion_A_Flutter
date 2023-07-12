9bf7a99cc10ef2ace1cbf56c5b17f7e31e3a21f5
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-07-12 15:06:06 -0600

    Separando logica en varios archivos

--- counter_app_01/lib/main.dart
+++ counter_app_01/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:counter_app_01/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(child: Text('Hola Mundo')),
+      debugShowCheckedModeBanner: false,
+      home: HomeSCreen(),
     );
   }
 }
