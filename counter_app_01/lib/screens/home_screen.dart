3eff97bb17e4c352d572da7ec5cd9d1de964fdc4
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-07-20 16:06:12 -0600

    Columnas

--- counter_app_01/lib/screens/home_screen.dart
+++ counter_app_01/lib/screens/home_screen.dart
@@ -7,9 +7,29 @@ class HomeSCreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // El Scaffold es un Widget que crea toda una pantalla en blanco para nosotros poder añadir estilos. Es como un lienzo para empezar a poner Widget
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        elevation: 8.0,
+      ),
       // backgroundColor: Colors.pink,
-      body: Center(child: Text('Hola Desde HomeScreen')),
+      // Cuando trabajamos con Scaffold siempre trabajamos en el body.
+      body: const Center(
+        child: Column(
+          // Propiedades de las columnas.
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            // Todo lo que esta aca, debe ser un widget
+            Text('Numero de Clicks'),
+            Text('10'),
+          ],
+        ),
+      ),
     );
   }
 }
+
+// Podemos trabajar con el widget Center, pero si no lo retiramos en su lugar y utilizamos las propiedades, mainAxisAligment, 
+
+// crossAxisAlignment: Es relativo al widget que tiene mayor tamaño
